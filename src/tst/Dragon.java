425feb6ee1a6c69e77cafaa288a27343f00b8344
rui <rui@rui-SAT.(none)>
2013-02-23 15:23:35 +0000

    saida OK

---
+++ src/tst/Dragon.java
@@ -0,0 +1,13 @@
+package tst;
+
+
+public class Dragon {
+	public int x,y;
+	public char symbol;
+	
+	public Dragon (int x, int y){
+		this.x=x;
+		this.y=y;
+		symbol='D';
+	}
+}
---
+++ src/tst/Hero.java
@@ -0,0 +1,15 @@
+package tst;
+
+
+public class Hero {
+	 public int x,y;
+	 public boolean haveSword;
+	 public char symbol;
+	 public Hero(int x, int y) {
+		this.x=x;
+		this.y=y;
+		haveSword=false;
+		symbol = 'H';
+	}
+	
+}
